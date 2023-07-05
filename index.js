3f806261300631c84cb4c1b2350c4d94fe8e41f5
SaumyaSrivastava <saumya2113061@akgec.ac.in>
2023-07-06 01:18:07 +0530

    Javascript Introduction

---
+++ index.js
@@ -0,0 +1,7 @@
+//what is javascript?
+//need-cleint side,server side,mobile development
+//ECMAScript-to create a scripting languauge(ECMA-262)
+//ES11 is the current edition 
+//ES6 is the popular one
+//use MDN for refernce for javascript
+//javascript enfine-V8 in Chrome,Monkey in Firefox,Chakra in edge
